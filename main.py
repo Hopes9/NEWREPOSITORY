c3cb19e8bdf78ba6c20b171ba3f91dee41b5eae1
Hopes9 <62842164+Hopes9@users.noreply.github.com>
2021-05-21 14:53:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import face_recognition
 
-known_image = face_recognition.load_image_file("/content/obama-480p.jpg")
+known_image = face_recognition.load_image_file("obama-480p.jpg")
 
-unknown_image_list = [face_recognition.load_image_file("/content/lin-manuel-miranda.png"), face_recognition.load_image_file("/content/2.jpg"), face_recognition.load_image_file("/content/obama.jpg")]
+unknown_image_list = [face_recognition.load_image_file("lin-manuel-miranda.png"), face_recognition.load_image_file("2.jpg"), face_recognition.load_image_file("obama.jpg")]
 
 for i in range(len(unknown_image_list)):
   biden_encoding = face_recognition.face_encodings(known_image)[0]
